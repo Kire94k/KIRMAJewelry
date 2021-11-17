c66d432024bd74f203b06a1a37c74b972504f537
Kirstine Emilie Hazen Jensen <kirs991k@edu.ucl.dk>
2021-11-17 09:59:37 +0100

    json files

--- KIRMAJewelry/Services/IBraceletService.cs
+++ KIRMAJewelry/Services/IBraceletService.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KIRMAJewelry.Models;
 
 namespace KIRMAJewelry.Services
 {
     interface IBraceletService
     {
+        Task<Bracelet[]> GetBracelets();
     }
 }
